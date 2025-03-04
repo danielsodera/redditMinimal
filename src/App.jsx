355c0e4b6389391a6ddaa9205c5ca5d874f1357d
Danielsodera <daniel_sodera@me.com>
2025-03-04 18:12:52 -0500

    Seperated header from App.jsx, and added it to it's own file, Header.jsx and Header.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
 import Card from './components/card/Card'
+import Header from './features/header/header'
+
 
 function App() {
 
   return (
     <>
     <header>
-      <h1>Reddit<strong>Minimal</strong></h1>
-      <img className='hamburger-btn' src='../src/assets/Hamburger_icon.png' alt='sub-reddit menu button' />
+      <Header /> 
     </header>
     <div className='app-body'>
           <Card /> 
---
+++ src/features/header/header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './header.css'
+
+const Header = () => {
+
+
+
+
+    return (
+        <div className="header-component">
+        <h1>Reddit<strong>Minimal</strong></h1>
+        <img className='hamburger-btn' src='../src/assets/Hamburger_icon.png' alt='sub-reddit menu button' />
+        </div>
+        
+    )
+}
+
+export default Header;
