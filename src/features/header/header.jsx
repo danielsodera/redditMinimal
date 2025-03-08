8cae5d5a96928a985f72436c4c913748d1e69163
Danielsodera <daniel_sodera@me.com>
2025-03-08 11:48:25 -0500

    Added search bar with state

--- src/features/header/header.jsx
+++ src/features/header/header.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import './header.css'
 
 const Header = () => {
 
+    const [searchTerm, setSearchTerm] = useState("")
+
+    const handleSearch = (e) => {
+        setSearchTerm(e.target.value)
+        console.log(searchTerm)
+    }
 
 
 
     return (
         <div className="header-component">
         <h1>Reddit<strong>Minimal</strong></h1>
+        <input name="search-bar" placeholder="search..." onChange={handleSearch} value={searchTerm}/> 
         <img className='hamburger-btn' src='../src/assets/Hamburger_icon.png' alt='sub-reddit menu button' />
         </div>
         
